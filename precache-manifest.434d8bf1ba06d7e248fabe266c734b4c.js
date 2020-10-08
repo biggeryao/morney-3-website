e8224d4b333d328fef024aa1fb601cd4f8d35986
joli <1040123159@qq.com>
2020-10-08 23:36:49 +0800

    deploy

---
+++ precache-manifest.434d8bf1ba06d7e248fabe266c734b4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b75cbd90590f9e17f7b",
+    "url": "/morney-3-website/css/app.46e6a747.css"
+  },
+  {
+    "revision": "7b6776280f558ce432352f5cc0e02c5b",
+    "url": "/morney-3-website/index.html"
+  },
+  {
+    "revision": "7b75cbd90590f9e17f7b",
+    "url": "/morney-3-website/js/app.5127a893.js"
+  },
+  {
+    "revision": "eba7d22c550b3a41787d",
+    "url": "/morney-3-website/js/chunk-vendors.468fbba8.js"
+  },
+  {
+    "revision": "32cedf0b5eb7a3c2520ebc3717085abf",
+    "url": "/morney-3-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-3-website/robots.txt"
+  }
+]);
